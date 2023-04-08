6bb1f5245f2edbd4121da5062a8385fae26b7853
RebornEnder <120225426+RebornEnder@users.noreply.github.com>
2023-04-08 11:08:50 -0700

    Add files via upload

---
+++ economy/docs/localdist/swagger-initializer.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    urls: [
+      {url: "json/v1.json", name: "Endpoint V1"},
+      {url: "json/v2.json", name: "Endpoint V2"},
+	  {url: "json/v3.json", name: "Endpoint V3"},
+    ],	
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
